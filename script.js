01ee7c5bc04dba0b54ebac27dfc42de6536c5225
Sanjiv39 <74110219+Sanjiv39@users.noreply.github.com>
2023-04-04 16:07:52 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,41 @@
+var str1 = ""
+var str2 = ""
+const check = () =>
+{
+    var user = username.value
+    var pass = password.value
+    console.log(user, pass)
+    if(user=="")
+    {
+        document.getElementById("username").style = `outline: none`
+    }
+    else if(user==="username")
+    {
+        document.getElementById("username").style = `outline: green solid 1px`
+    }
+    else
+    {
+        document.getElementById("username").style = `outline: red solid 1px`
+    }
+    if(pass=="")
+    {
+        document.getElementById("password").style = `outline: none`
+    }
+    else if(pass==="password")
+    {
+        document.getElementById("password").style = `outline: green solid 1px`
+    }
+    else
+    {
+        document.getElementById("password").style = `outline: red solid 1px`
+    }
+}
+
+submitbtn.onclick = () => 
+{
+    document.getElementById("circle1").style = `animation: slide1 1s linear infinite alternate;`
+    document.getElementById("circle2").style = `animation: slide1 1s linear infinite alternate;`
+    setTimeout(() => {
+        location.reload()
+    }, 3000);
+}
